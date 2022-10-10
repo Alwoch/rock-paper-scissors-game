aa7b486fccb24c103d2233db7fad44433895f063
salwoch <>
2022-10-10 15:19:43 +0000

    refactor():check_win function

--- main.py
+++ main.py
@@ -14,27 +14,27 @@ def get_choices():
   return choices
 
 
-play = get_choices()
-print(play)
-
-def check_win(player,computer):
+def check_win(player, computer):
   print(f"you chose {player} and the computer chose {computer}")
-  if player==computer:
+  if player == computer:
     return "its a tie!"
-  elif player=="rock":
-    if computer=="scissors":
+  elif player == "rock":
+    if computer == "scissors":
       return "rock smashes scissors, you win!"
     else:
       return "paper covers rock, you lose!"
-  elif player=="paper":
-    if computer=="rock":
+  elif player == "paper":
+    if computer == "rock":
       return "paper covers rock,you win!"
     else:
       return "sccissor cuts paper,you lose!"
-  elif player=="scissors":
-    if computer=="paper":
+  elif player == "scissors":
+    if computer == "paper":
       return "scissor cuts paper,you win!"
     else:
       return "rock smashes scissors,you lose!"
 
 
+choices = get_choices()
+result = check_win(choices["player"], choices['computer'])
+print(result)
