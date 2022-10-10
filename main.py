88b4114a7533184764c2c5806cc98032ca5cc932
Alwoch Sophia <salwoch@gmail.com>
2022-10-10 15:01:11 +0000

    add rock-paper-scissors game

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import random
 
 
 #declaring functions and variables
+#dictionaries:store data values in key value pairs
+#input function allows a user prompt
+#outputting strings can be cone either by concatenation or by using fstrings
 def get_choices():
   player_choice = input("Enter a choice(rock,paper,scissors)")
   options = ["rock", "paper", "scissors"]
@@ -13,9 +16,25 @@ def get_choices():
 
 play = get_choices()
 print(play)
-#dictionaries:store data values in key value pairs
-#input function allows a user prompt
-#outputting strings can be cone either by concatenation or by using fstrings eg
 
-age = 20
-print(f"Jim is {age} years old")
+def check_win(player,computer):
+  print(f"you chose {player} and the computer chose {computer}")
+  if player==computer:
+    return "its a tie!"
+  elif player=="rock":
+    if computer=="scissors":
+      return "rock smashes scissors, you win!"
+    else:
+      return "paper covers rock, you lose!"
+  elif player=="paper":
+    if computer=="rock":
+      return "paper covers rock,you win!"
+    else:
+      return "sccissor cuts paper,you lose!"
+  elif player=="scissors":
+    if computer=="paper":
+      return "scissor cuts paper,you win!"
+    else:
+      return "rock smashes scissors,you lose!"
+
+
